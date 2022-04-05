a2fa636da7f1b8926a145b441ca9dcf526bd1328
MAZeferino <matheus.zeferino@euax.com.br>
2022-04-05 13:51:37 -0300

    Adicionando javascript

---
+++ index.js
@@ -0,0 +1,8 @@
+function teste(){
+    
+    const name = document.getElementById('name').value
+    const mail = document.getElementById('mail').value
+    const area = document.getElementById('area').value
+
+    return alert(`Woooooooooow \n \n Seja bem-vindo ${name} ao bootcamp da àrea de ${area}. \n \n Você se cadastrou com o email \n ${mail}`)
+}
